ef0b6cba896cfcb373da823a58a3000353f46821
ogkisque <142596280+ogkisque@users.noreply.github.com>
2023-08-20 20:59:48 +0500

    Add an input check

--- kvadrat.cpp
+++ kvadrat.cpp
@@ -1,22 +1,45 @@
 #include <stdio.h>
 #include <TXLib.h>
 #include <math.h>
+#include <string.h>
+#include <stdlib.h>
 #define POGR 0.00001
+
 int main()
 {
     double a, b, c;
-    printf("Введите коэффициенты a, b, c\n");
-    scanf("%lf %lf %lf", &a, &b, &c);
+
+
+    printf("Enter the coefficient a\n");
+    while (!(scanf("%lf", &a) == 1 && getchar() == '\n'))
+    {
+        while (getchar() != '\n');
+        printf("Enter the coefficient a\n");
+    }
+    printf("Enter the coefficient b\n");
+    while (!(scanf("%lf", &b) == 1 && getchar() == '\n'))
+    {
+        while (getchar() != '\n');
+        printf("Enter the coefficient b\n");
+    }
+    printf("Enter the coefficient c\n");
+    while (!(scanf("%lf", &b) == 1 && getchar() == '\n'))
+    {
+        while (getchar() != '\n');
+        printf("Enter the coefficient c\n");
+    }
+
+
     if (a <= POGR)
     {
         if (b <= POGR && c <= POGR)
         {
-            printf("Выражение не является уравнением");
+            printf("The expression is not an equation");
         }
         else
         {
             double x = -1 * c / b;
-            printf("Одно решение: %.5lf", x);
+            printf("One solution: %.5lf", x);
         }
     }
     else
@@ -24,18 +47,18 @@ int main()
         double des = b*b - 4*a*c;
         if (des < 0)
         {
-            printf("Нет решений");
+            printf("No solutions");
         }
         else if (des <= POGR)
         {
             double x = -1 * b / (2*a);
-            printf("Одно решение: %.5lf", x);
+            printf("One solution: %.5lf", x);
         }
         else
         {
             double x1 = (-1 * b - sqrt(des)) / (2*a);
             double x2 = (-1 * b + sqrt(des)) / (2*a);
-            printf("Два решения: %.5lf %.5lf", x1, x2);
+            printf("Two solutions: %.5lf %.5lf", x1, x2);
         }
     }
 }
