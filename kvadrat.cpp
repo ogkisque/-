c0a4cc0ac2cfec560547f3311abebf63ff1b9667
ogkisque <142596280+ogkisque@users.noreply.github.com>
2023-08-22 21:01:17 +0500

    с вводом 3 чисел одной функцией

--- kvadrat.cpp
+++ kvadrat.cpp
@@ -1,64 +1,138 @@
 #include <stdio.h>
-#include <TXLib.h>
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
-#define POGR 0.00001
+#include <assert.h>
 
-int main()
+const double EPS = 1e-5;
+
+void input_double (double* a, double* b, double* c);
+bool is_equal (double a, double b);
+void output_answer (int n_roots, double x1, double x2);
+int solve_equation (double a, double b, double c, double* x1, double* x2);
+int solve_linear (double a, double b, double* x);
+int solve_square (double a, double b, double c, double* x1, double* x2);
+bool end_input (char last_c);
+
+enum n_solutions
 {
-    double a, b, c;
+    ZERO, ONE, TWO, INFINIT = 1000
+};
 
+int main ()
+{
+    printf ("# Hello! This program will solve your quadratic equation.\n");
+    double a = 0, b = 0, c = 0;
+    input_double (&a, &b, &c);
 
-    printf("Enter the coefficient a\n");
-    while (!(scanf("%lf", &a) == 1 && getchar() == '\n'))
-    {
-        while (getchar() != '\n');
-        printf("Enter the coefficient a\n");
-    }
-    printf("Enter the coefficient b\n");
-    while (!(scanf("%lf", &b) == 1 && getchar() == '\n'))
-    {
-        while (getchar() != '\n');
-        printf("Enter the coefficient b\n");
-    }
-    printf("Enter the coefficient c\n");
-    while (!(scanf("%lf", &b) == 1 && getchar() == '\n'))
-    {
-        while (getchar() != '\n');
-        printf("Enter the coefficient c\n");
-    }
+    double x1 = 0, x2 = 0;
+    int n_roots = solve_equation (a, b, c, &x1, &x2);
 
+    output_answer (n_roots, x1, x2);
+}
 
-    if (a <= POGR)
+int solve_equation (double a, double b, double c, double* x1, double* x2)
+{
+    assert (isfinite(a));
+    assert (isfinite(b));
+    assert (isfinite(c));
+    assert (x1 != NULL);
+    assert (x2 != NULL);
+    assert (x1 != x2);
+
+    if (is_equal(a, 0))
     {
-        if (b <= POGR && c <= POGR)
-        {
-            printf("The expression is not an equation");
-        }
-        else
-        {
-            double x = -1 * c / b;
-            printf("One solution: %.5lf", x);
-        }
+        return solve_linear (b, c, x1);
     }
     else
     {
-        double des = b*b - 4*a*c;
-        if (des < 0)
-        {
-            printf("No solutions");
-        }
-        else if (des <= POGR)
-        {
-            double x = -1 * b / (2*a);
-            printf("One solution: %.5lf", x);
-        }
-        else
-        {
-            double x1 = (-1 * b - sqrt(des)) / (2*a);
-            double x2 = (-1 * b + sqrt(des)) / (2*a);
-            printf("Two solutions: %.5lf %.5lf", x1, x2);
-        }
+        return solve_square (a, b, c, x1, x2);
     }
+}
+
+int solve_linear (double a, double b, double* x)
+{
+    assert(isfinite(a));
+    assert(isfinite(b));
+    assert (x != NULL);
+
+    if (is_equal(a, 0))
+    {
+        if (is_equal(b, 0))
+            return INFINIT;
+        else
+            return ZERO;
+    }
+    else
+    {
+        *x = -b / a;
+        return ONE;
+    }
+}
+
+int solve_square (double a, double b, double c, double* x1, double* x2)
+{
+    assert (a != 0);
+    assert (x1 != NULL);
+    assert (x2 != NULL);
+    assert (x1 != x2);
+    double des = b*b - 4*a*c;
+    if (des < -EPS)
+    {
+        return ZERO;
+    }
+    else if (is_equal(des, 0))
+    {
+        *x1 = -b / (2*a);
+        return ONE;
+    }
+    else
+    {
+        double sqrt_des = sqrt(des);
+        *x1 = (-b - sqrt_des) / (2*a);
+        *x2 = (-b + sqrt_des) / (2*a);
+        return TWO;
+    }
+}
+
+void output_answer(int n_roots, double x1, double x2)
+{
+    switch (n_roots) {
+    case ZERO:
+        printf ("No solutions");
+        break;
+    case ONE:
+        printf ("1 solution: %.5lf", x1);
+        break;
+    case TWO:
+        printf ("2 solutions: %.5lf %.5lf", x1, x2);
+        break;
+    case INFINIT:
+        printf ("Infinite number of solutions");
+        break;
+    default:
+        assert (0);
+        break;
+    }
+}
+
+void input_double (double* a, double* b, double* c)
+{
+    printf ("# Enter the coefficients a, b, c separated by a space\n");
+    while (!(scanf("%lf %lf %lf", a, b, c) == 3))
+    {
+        while (getchar() != '\n')
+            ;
+        printf ("# Invalid input\n# Enter the coefficients a, b, c separated by a space\n");
+    }
+}
+
+bool end_input (char last_c)
+{
+    return (last_c == '\n' || last_c == ' ' || last_c == '\t');
+}
+
+bool is_equal (double a, double b)
+{
+    return ((a - b) <= EPS);
 }
